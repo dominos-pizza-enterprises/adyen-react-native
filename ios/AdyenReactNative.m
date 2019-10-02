5237db7997afb7557adf6f3ea3f9558a7fbda56e
Joe Patterson <jrwpatterson@gmail.com>
2019-10-02 13:28:39 +1000

    remove files

--- ios/AdyenReactNative.m
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-@interface RCT_EXTERN_MODULE(AdyenReactNative, NSObject)
-    RCT_EXTERN_METHOD(startPayment)
-    RCT_EXTERN_METHOD(confirmPayment:(NSString)encodedToken)
-    RCT_EXTERN_METHOD(initPayment)
-    RCT_EXTERN_METHOD(cancelPayment)
-@end
